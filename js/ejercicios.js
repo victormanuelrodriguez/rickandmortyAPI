d3f3308a5d92e57c7c49aaab989cda362f65fca3
victormanuelrodriguez <victor.rodriguez@holasoft.es>
2020-10-29 14:18:56 +0000

    Correción de ejercicio 5 y 6. Añadido el ejercicio 7 y empezando el 8

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,6 +1,6 @@
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
+const fs = require('fs');
 /**
  * Obtener listado de humanos que aparecen en el episodio 25
  *  
@@ -8,20 +8,51 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 async function ejercicio5 () {
     const url = 'https://rickandmortyapi.com/api/character/?species=human';
     const method ='GET';
+    var sum = 0;
     var result = [];
+    var character = [];
     await connect(method, url)
         .then(respuesta => {
             var res = JSON.parse(respuesta);
-            result = res.results;
+            result = res;
         })
-        .catch( error => console.log(error))
-    result.forEach(e=>{
-        e.episode.forEach(a=>{
+        .catch( error => console.log(error));
+    sum = result.results.length;
+        result.results.forEach(e=>{
+            character.push(e);
+        })
+    for(i=0; i <= result.info.pages ;i++){
+        if(i > 1){
+            var url_ = `https://rickandmortyapi.com/api/character/?page=${i}&species=human`;
+            await connect(method, url_).then(
+                respuesta => {
+                    var res = JSON.parse(respuesta);
+                    res.results.forEach(e => {
+                        character.push(e);
+                    })
+                    sum = sum + res.results.length;
+                }
+            )
+        }
+    }
+    // console.log("suma final:", sum)
+    // console.log("tamaño personajes:",character.length);
+    characterCap = [];
+    character.forEach((e) => {
+        e.episode.forEach(a => {
             if (a.includes('/25')) {
-                console.log(e);
+                // console.log(e.name);
+                characterCap.push(e);
             }
         });
     });
+    /* var stream = fs.createWriteStream("file.txt");
+    stream.once('open', function (fd) {
+        stream.write(JSON.stringify(character));
+        stream.end();
+    }); */
+    console.log(characterCap);
+    console.log("Personajes:", characterCap.length);
 }
 /**
  * Con el listado obtenido de personas del episodio 5, muestra los humanos
@@ -29,20 +60,114 @@ async function ejercicio5 () {
 async function ejercicio6() {
     const url = 'https://rickandmortyapi.com/api/character/?species=human';
     const method = 'GET';
+    var sum = 0;
+    var result = [];
+    var character = [];
+    await connect(method, url)
+        .then(respuesta => {
+            var res = JSON.parse(respuesta);
+            result = res;
+        })
+        .catch(error => console.log(error));
+    sum = result.results.length;
+    result.results.forEach(e => {
+        character.push(e);
+    })
+    for (i = 0; i <= result.info.pages; i++) {
+        if (i > 1) {
+            var url_ = `https://rickandmortyapi.com/api/character/?page=${i}&species=human`;
+            await connect(method, url_).then(
+                respuesta => {
+                    var res = JSON.parse(respuesta);
+                    res.results.forEach(e => {
+                        character.push(e);
+                    })
+                    sum = sum + res.results.length;
+                }
+            )
+        }
+    }
+    // console.log("suma final:", sum)
+    // console.log("tamaño personajes:",character.length);
+    characterCap = [];
+    character.forEach((e) => {
+        e.episode.forEach(a => {
+            if (a.includes('/5')) {
+                // console.log(e.name);
+                characterCap.push(e);
+            }
+        });
+    });
+    /* var stream = fs.createWriteStream("file.txt");
+    stream.once('open', function (fd) {
+        stream.write(JSON.stringify(character));
+        stream.end();
+    }); */
+    console.log(characterCap);
+    console.log("Personajes:", characterCap.length);
+}
+/**
+ * Muestra un listado de sitios que aparecen en el episodio 20
+ * Envía una petición a la API para recoger a todos los personajes.
+ * Espera a una promesa y Obtiene los lugares
+ */
+async function ejercicio7() {
+    const url = 'https://rickandmortyapi.com/api/episode/20';
+    const method = 'GET';
     var result = [];
     await connect(method, url)
         .then(respuesta => {
             var res = JSON.parse(respuesta);
-            result = res.results;
+            result = res.characters;
         })
-        .catch(error => console.log(error))
-    result.forEach(e => {
-        e.episode.forEach(a => {
-            if (a.includes('/5')) {
-                console.log(e);
+        .catch(error => console.log(error));
+    var character = [];
+    for (let index = 0; index < result.length; index++) {
+        console.log(result[index]);
+        await connect(method, result[index]).then(
+            res => {
+                var respuesta = JSON.parse(res);
+                character.push(respuesta);
+
             }
-        });
+        ).catch(error => console.log(error))
+    }
+    var location = [];
+    character.forEach((e) => {
+        location.push(e.location);
     });
+    console.log(location);
+}
+/**
+ * Del listado de sitios obtenidos del episodio 20, 
+ * muestra los siguientes datos: Lugares de origen de cada personaje y 
+ * listado de episodios en los que aparecen las distintos sitios que aparecen en este episodio.
+ */
+async function ejercicio8() {
+    const url = 'https://rickandmortyapi.com/api/episode/20';
+    const method = 'GET';
+    var result = [];
+    await connect(method, url)
+        .then(respuesta => {
+            var res = JSON.parse(respuesta);
+            result = res.characters;
+        })
+        .catch(error => console.log(error));
+    var character = [];
+    for (let index = 0; index < result.length; index++) {
+        await connect(method, result[index]).then(
+            res => {
+                var respuesta = JSON.parse(res);
+                character.push(respuesta);
+
+            }
+        ).catch(error => console.log(error))
+    }
+    var location = [];
+    character.forEach((e, i = 0) => {
+        location.push(e.location);
+    });
+    console.log(location);
 }
 
 async function connect(method,url){
@@ -69,4 +194,4 @@ async function connect(method,url){
     });
 }
 
-ejercicio6()
+ejercicio7()
